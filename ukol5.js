e26fb47032c1f2701a78033eef64c9aa4eb7f95e
ivanazacina <ivana.plasienkova@gmail.com>
2022-01-22 13:24:37 +0100

    1 file modified, 1 added

--- ukol5.js
+++ ukol5.js
@@ -5,7 +5,23 @@ let vekOdpoved = vek;
 console.log(jmenoOdpoved);
 console.log(vekOdpoved);
 if (vek > 18) {
-  console.log('Muzes riditi auto.');
+  console.log('Muzes ridit auto.');
 } else {
   console.log('Nemuzes ridit auto. Muzes ho ridit za ' + (18 - vek) + 'let. ');
+}
+
+// pokus cislo dva podla andy
+let vaseJmeno = prompt('JAk se jmenujes?');
+console.log(vaseJmeno);
+let vasVek = prompt('Kolike je ti let?');
+console.log(vasVek);
+
+if (vasVek >= 18) {
+  console.log(
+    `Tvoje jmeno je ${vaseJmeno}, je ti ${vasVek} a muzes ridit auto. `
+  );
+} else {
+  console.log(
+    `Tvoje jmeno je ${vaseJmeno}, tvuj vek je ${vasVek} a tak nemuzes ridit auto. `
+  );
 }
